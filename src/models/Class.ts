a94352a9bc94ada1ee2cd0c1834e34284176878e
= <=>
2024-10-13 09:58:51 +0300

    Building a model for a class

---
+++ src/models/Class.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+export interface IClass extends Document {
+  name: string;
+  teacher: Types.ObjectId;
+  students: Types.ObjectId[];
+}
+
+const ClassSchema = new Schema<IClass>({
+  name: {
+    type: String,
+    required: [true, "name is required"],
+    unique: true,
+    minlength: [3, "name must be at least 3 characters long"],
+    maxlength: [30, "name cannot exceed 30 characters"],
+    match: [/^[a-zA-Z0-9_]+$/, "Username can only contain letters, numbers, and underscores"],
+  },
+  
+  
+  teacher: { type: Schema.Types.ObjectId, ref: "Teacher" },
+  students: [{ type: Schema.Types.ObjectId, ref: "Student" }],
+});
+
+
+export default mongoose.model<IClass>("Teacher",ClassSchema );
