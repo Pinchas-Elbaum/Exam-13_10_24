5b391f3854b1f1e8e0082257cb492dc6b80ce9d6
= <=>
2024-10-13 09:54:11 +0300

    Building a model for a teacher

---
+++ src/models/Teacher.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+import validator from "validator";
+
+export interface ITeacher extends Document {
+  name: string;
+  email: string;
+  password: string;
+  class: Types.ObjectId;
+}
+
+const TeacherSchema = new Schema<ITeacher>({
+  name: {
+    type: String,
+    required: [true, "name is required"],
+    unique: true,
+    minlength: [3, "name must be at least 3 characters long"],
+    maxlength: [30, "name cannot exceed 30 characters"],
+    match: [/^[a-zA-Z0-9_]+$/, "Username can only contain letters, numbers, and underscores"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    validate: {
+      validator: function (value: string) {
+        return validator.isEmail(value);
+      },
+      message: "Please provide a valid email address",
+    },
+  },
+  
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    select: false,
+  },
+  class: { type: Schema.Types.ObjectId, ref: "Class" },
+});
+
+export default mongoose.model<ITeacher>("Teacher",TeacherSchema );
